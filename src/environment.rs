55f6eeb4bcfaf73a8d411973ce644f9804ee090c
yuri <services@yurirenko.com>
2024-01-10 22:49:30 +0900

    Add more tests and fix the issue with shadowing not working

--- src/environment.rs
+++ src/environment.rs
@@ -19,13 +19,16 @@ impl Environment {
     }
 
     pub fn assign(&mut self, name: &str, v: LiteralValue) {
-        if let Some(enclosing_env) = &mut self.enclosing {
-            enclosing_env.assign(name, v);
-        } else {
-            if self.values.contains_key(name) {
+        match self.values.contains_key(name) {
+            true => {
                 self.values.insert(name.to_string(), v);
-            } else {
-                panic!("Variable {} is not defined", name);
+            }
+            false => {
+                if let Some(enclosing_env) = &mut self.enclosing {
+                    enclosing_env.assign(name, v);
+                } else {
+                    panic!("Variable {} is not defined", name);
+                }
             }
         }
     }
@@ -55,22 +58,18 @@ fn test_define() {
 }
 
 #[test]
-fn test_assign() {
-    let number_val = 3.147;
-    let expected_value = LiteralValue::Number(number_val);
+fn test_define_enclosed() {
+    let expected_value = LiteralValue::Number(3.14);
+    let mut enclosed_env = Box::new(Environment::new(None));
+    enclosed_env.define("pi", expected_value.clone());
 
-    let mut env = Environment::new(None);
-
-    env.define("pi", LiteralValue::Number(3.14));
-    env.assign("pi", expected_value.clone());
-
+    let env = Environment::new(Some(enclosed_env));
     assert_eq!(env.get("pi"), expected_value);
 }
 
 #[test]
 fn test_define_enclosed_no_value() {
-    let number_val = 3.14;
-    let expected_value = LiteralValue::Number(number_val);
+    let expected_value = LiteralValue::Number(3.14);
     let enclosed_env = Box::new(Environment::new(None));
 
     let mut env = Environment::new(Some(enclosed_env));
@@ -99,4 +98,60 @@ fn test_define_enclosed_with_shadowing() {
 
     let enclosed_env = env.enclosing.unwrap();
     assert_eq!(enclosed_env.get("pi"), shadowed_value);
+}
+
+#[test]
+fn test_assign() {
+    let number_val = 3.147;
+    let expected_value = LiteralValue::Number(number_val);
+
+    let mut env = Environment::new(None);
+
+    env.define("pi", LiteralValue::Number(3.14));
+    env.assign("pi", expected_value.clone());
+
+    assert_eq!(env.get("pi"), expected_value);
+}
+
+#[test]
+#[should_panic]
+fn test_assign_no_value() {
+    let number_val = 3.147;
+    let expected_value = LiteralValue::Number(number_val);
+
+    let mut env = Environment::new(None);
+
+    env.define("pi", expected_value);
+    env.assign("tau", LiteralValue::Number(6.28));
+}
+
+#[test]
+fn test_assign_enclosed() {
+    let shadowed_value = LiteralValue::Number(3.14);
+    let mut enclosed_env = Box::new(Environment::new(None));
+
+    enclosed_env.define("pi", shadowed_value.clone());
+
+    let new_value = LiteralValue::Number(10.0);
+
+    let mut env = Environment::new(Some(enclosed_env));
+    env.assign("pi", new_value.clone());
+
+    assert_eq!(env.get("pi"), new_value);
+}
+
+#[test]
+fn test_assign_enclosed_with_shadowing() {
+    let shadowed_value = LiteralValue::Number(3.14);
+    let mut enclosed_env = Box::new(Environment::new(None));
+
+    enclosed_env.define("pi", shadowed_value.clone());
+
+    let new_value = LiteralValue::Number(10.0);
+
+    let mut env = Environment::new(Some(enclosed_env));
+    env.define("pi", shadowed_value.clone());
+    env.assign("pi", new_value.clone());
+
+    assert_eq!(env.get("pi"), new_value);
 }
